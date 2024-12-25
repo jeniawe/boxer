3017280db58b51aae1edfe772a5ba38481771447
jenia <maksyuta2909@gmail.com>
2024-12-25 16:38:23 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Зупиняє перезавантаження сторінки
+  alert('Дякуємо за ваш відгук! Ми зв’яжемося з вами найближчим часом.');
+});
